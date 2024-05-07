77e5eed01b01c82d6931e121fc094aca472d5ca1
Torycd <vicidundun@gmail.com>
2024-05-07 19:04:31 +0100

    lets go

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNavigation = () => {
+    setNav(!nav);
+  };
   return (
     <div className="fixed w-full h-20 shadow-xl z-100">
-      <div className="flex justify-between items-center max-w-[1240px] h-full  2xl:px-16">
+      <div className="flex justify-between items-center max-w-[1240px] px-2 mx-auto h-full  2xl:px-16">
         {/* logo */}
-        <div className="w-12 h-12 relative">
+        <div className="w-32 h-16 relative">
           <Link href="/">
             <Image src="/assets/Logo.png" alt="Victory's Image" layout="fill" />
           </Link>
         </div>
         {/* Other content */}
         <div>
-          <ul>
+          <ul className="hidden md:flex">
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
@@ -35,6 +45,95 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
+          <div onClick={handleNavigation} className="md:hidden">
+            <AiOutlineMenu size={30} />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in h-screen  duration-500"
+          }
+        >
+          {/* nav top */}
+          <div className="flex w-full items-center justify-between">
+            {/* logo */}
+            <div className="w-16 h-12 relative">
+          <Link href="/">
+            <Image src="/assets/Logo.png" alt="Victory's Image" layout="fill" />
+          </Link>
+        </div>
+            {/* close home */}
+            <div
+              onClick={handleNavigation}
+              className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+            >
+              <AiOutlineClose />
+            </div>
+          </div>
+          {/* statement */}
+          <div className="border-b border-gray-300 my-4">
+            <p className="w-[85%] md:w-[90%] py-4">
+              Let&apos;s build something together
+            </p>
+          </div>
+          {/* nav item */}
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
+              </Link>
+              <Link href="/#skills">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#projects">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#contacts">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Contacts
+                </li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Lets Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[20%]">
+                <a className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedinIn />
+                </a>
+                <a className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+                </a>
+                <a className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+                </a>
+                <a className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
