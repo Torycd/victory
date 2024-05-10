fe917c1bb1c6dcf59b5f0efbc2ffdf219c7716a0
Torycd <vicidundun@gmail.com>
2024-05-10 07:41:46 +0100

    hell yes

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ProjectItem from "@/components/ProjectItem";
+import Image from "next/image";
+// import ProjectItem from "@/components/ProjectItem";
 import project1 from "@/public/assets/project/project1.jpg"
 import project2 from "@/public/assets/project/project2.jpg"
 import project3 from "@/public/assets/project/project3.jpg"
@@ -10,26 +11,9 @@ const Projects = () => {
     <div className="w-full 2xl:px-16 pt-32 px-2">
       <div className="mx-auto px-2 mt-10">
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="Ip Address Finder"
-            backgroundImg={project1}
-            projectUrl="/projects/ipaddress"
-          />
-           <ProjectItem
-            title="Shortly"
-            backgroundImg={project2}
-            projectUrl="/projects/shortly"
-          />
-           <ProjectItem
-            title="Netflix App"
-            backgroundImg={project3}
-            projectUrl="/projects/netflix"
-          />
-           <ProjectItem
-            title="Twitch App"
-            backgroundImg={project4}
-            projectUrl="/projects/twitch"
-          />
+          <div>
+            <Image className="rounded-lg" src={project1} alt="" />
+          </div>
         </div>
       </div>
     </div>
