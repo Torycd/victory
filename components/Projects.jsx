066ef25296114f84fffaff57fec80d2060c1f06f
Torycd <vicidundun@gmail.com>
2024-05-10 22:56:25 +0100

    f

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import React from "react";
 import netflix from "../public/assets/project/project0.JPG";
 import ipaddress from "../public/assets/project/project1.JPG";
+import shortly from "../public/assets/project/project2.JPG";
+import loopstudio from "../public/assets/project/project3.JPG";
+import space from "../public/assets/project/project4.JPG";
 
 const Projects = () => {
   return (
@@ -9,6 +12,7 @@ const Projects = () => {
       <div className="max-w-[1200px] mx-auto space-y-10">
         <h2 className="text-blue-700">Porfolio</h2>
         <h2>Each project is a unique piece of developmentt</h2>
+        {/* project1 */}
         <div className="grid md:grid-cols-2 gap-5">
           {/* image */}
           <div className="col-span-1">
@@ -28,6 +32,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
+        {/* project 2 */}
         <div className="grid md:grid-cols-2 gap-5">
           {/* image */}
           <div className="col-span-1">
@@ -63,15 +68,16 @@ const Projects = () => {
               <p>API</p>
             </div>
             {/* image */}
-            <div className="col-span-1">
-              <Image
-                src={ipaddress}
-                className="rounded-lg"
-                alt="Ip Address finderss"
-              />
-            </div>
+          </div>
+          <div className="col-span-1">
+            <Image
+              src={ipaddress}
+              className="rounded-lg"
+              alt="Ip Address finderss"
+            />
           </div>
         </div>
+        {/* project  */}
         <div className="grid md:grid-cols-2 gap-5">
           {/* image */}
           <div className="col-span-1">
@@ -95,14 +101,6 @@ const Projects = () => {
           </div>
         </div>
         <div className="grid md:grid-cols-2 gap-5">
-          {/* image */}
-          <div className="col-span-1">
-            <Image
-              src={ipaddress}
-              className="rounded-lg"
-              alt="Ip Address finderss"
-            />
-          </div>
           {/* Details of Projects */}
           <div className="col-span-1">
             <h2>IP Address Finder</h2>
@@ -114,6 +112,14 @@ const Projects = () => {
               <p>ReactJs</p>
               <p>API</p>
             </div>
+          </div>
+          {/* image */}
+          <div className="col-span-1">
+            <Image
+              src={ipaddress}
+              className="rounded-lg"
+              alt="Ip Address finderss"
+            />
           </div>
         </div>
       </div>
