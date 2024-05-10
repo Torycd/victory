04ac8e193e461a2c86fab4a7f00b92678b00499a
Torycd <vicidundun@gmail.com>
2024-05-10 23:05:34 +0100

    ha

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import About from "@/components/About";
 import Main from "@/components/Main";
-import Projects from "../components/projects";
+import Projects from "../components/Projects.jsx";
 
 export default function Home() {
   return (
