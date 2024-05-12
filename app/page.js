5abc5234d4550a4636b8f232ffa9db529f33df6e
Torycd <vicidundun@gmail.com>
2024-05-12 16:51:19 +0100

    lets go

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Main from "@/components/Main";
 import Projects from "../components/Projects.jsx";
+import Footer from "@/components/Footer.jsx";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Main />
       <About />
       <Projects/>
+      <Footer/>
     </div>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full'>
+        <h2>footer</h2>
+    </div>
+  )
+}
+
+export default Footer
